b677a33a94864d169684fc0b46096cdc80522d95
Wellington <WellingonMarinho>
2021-01-25 15:40:37 -0300

    Additing filter with Regex in search.

--- js/filtra.js
+++ js/filtra.js
@@ -4,14 +4,20 @@ let campoFiltro = document.querySelector('#filtrar-tabela')
 campoFiltro.addEventListener('input', function(){
     let pacientes = document.querySelectorAll('.paciente')
     if (this.value.length > 0){
+      console.log(this.value, this.value.length)
+      console.log('Entrou no if e consequentemente no forEach')
         pacientes.forEach(function(paciente){
             pacienteNome = paciente.querySelector('.info-nome').textContent
-
-            if (pacienteNome != campoFiltro.value){
+            let nomeRegex = new RegExp(campoFiltro.value, "i")
+            if (!nomeRegex.test(pacienteNome)){
                 paciente.classList.add('invisivel')
             }else{
               paciente.classList.remove('invisivel')
             }
         })
-    })
-}
+    }else{
+          pacientes.forEach(function(paciente){
+          paciente.classList.remove('invisivel')
+        })
+    }
+})
